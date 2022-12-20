2ca8ab78ee56d6ab14e0adb0d05028d891c2c447
Liam Howley <liam.howley@hotmail.com>
2022-12-20 23:14:15 +0000

    testing for failures with multiple attributes

--- test/tests.lisp
+++ test/tests.lisp
@@ -75,50 +75,49 @@
 
 (defvar *markup* "<div id='container1' class='container square'><a href=\"/test\" target=\"_blank\"><span class='square'>caption</span><img src='/my-img-server'/>><</a></div>")
 
-(defvar *parsed-markup* (parse-document (make-instance 'xml-document-node :document *markup*)))
-
 (define-test parse-markup...
   :parent test-parse
-    (of-type 'xml-document-node *parsed-markup*)
-  (of-type 'element-node (car (slot-value *parsed-markup* 'child-nodes)))
-  (of-type 'div (get-element-with-attribute *parsed-markup* "id"))
-  (of-type 'div (get-element-with-attribute-value *parsed-markup* "id" "container1"))
-  ;; this is xml where attribute values can only be a string. This should be false
-  (let ((element (get-element-with-attribute-value *parsed-markup* "class" "container")))
-    (false (typep element 'div)))
-  (let ((element (get-element-with-attribute-value *parsed-markup* "class" "container square")))
-    (true (typep element 'div)))
-  (let ((elements (get-elements-with-attribute-value *parsed-markup* "class" "square")))
-    (of-type 'span (car elements)))
-  (let ((elements (get-elements-with-attribute-values *parsed-markup* "class" "square" "container")))
-    (of-type 'span (car elements)))
-  (let ((text-nodes (retrieve-text-nodes *parsed-markup*)))
-    (is string= "caption" (text (car text-nodes)))
-    (is string= ">" (text (cadr text-nodes)))
-    (is string= "<" (text (caddr text-nodes)))
-    (of-type 'span (parent-node (car text-nodes)))
-    (of-type 'img (get-next-sibling (parent-node (car text-nodes)))))
-  (is string= "caption" (text (car (retrieve-text-nodes-with-token *parsed-markup* "cap"))))
-  (is string= "caption" (text (car (retrieve-text-nodes-with-tokens *parsed-markup* "cap" "tion")))))
+  (fail (parse-document (make-instance 'xml-document-node :document *markup*)) 'multiple-value-error)
+  (let ((parsed-markup (handler-bind ((multiple-value-error #'use-first-found-value))
+			 (parse-document (make-instance 'xml-document-node :document *markup*)))))
+    (of-type 'xml-document-node parsed-markup)
+    (of-type 'element-node (car (slot-value parsed-markup 'child-nodes)))
+    (of-type 'div (get-element-with-attribute parsed-markup "id"))
+    (of-type 'div (get-element-with-attribute-value parsed-markup "id" "container1"))
+    ;; this is xml where attribute values can only be a string. This should be false
+    (let ((element (get-element-with-attribute-value parsed-markup "class" "container")))
+      (true (typep element 'div)))
+    (let ((element (get-element-with-attribute-value parsed-markup "class" "square")))
+      (of-type 'span element))
+    (let ((text-nodes (retrieve-text-nodes parsed-markup)))
+      (is string= "caption" (text (car text-nodes)))
+      (is string= ">" (text (cadr text-nodes)))
+      (is string= "<" (text (caddr text-nodes)))
+      (of-type 'span (parent-node (car text-nodes)))
+      (of-type 'img (get-next-sibling (parent-node (car text-nodes)))))
+    (is string= "caption" (text (car (retrieve-text-nodes-with-token parsed-markup "cap"))))
+    (is string= "caption" (text (car (retrieve-text-nodes-with-tokens parsed-markup "cap" "tion"))))))
 
 
 
 (define-test serialize...
   :parent test-parse
-  (is string= "<div id='container1' class='container square'><a href='/test' target='_blank'><span class='square'>caption</span><img src='/my-img-server' />&gt;&lt;</a></div>" (serialize *parsed-markup*))
-  (is string=  
-      "<div id='container1' class='container square'>
+  (let ((parsed-markup (handler-bind ((multiple-value-error #'use-first-found-value))
+			 (parse-document (make-instance 'xml-document-node :document *markup*)))))
+    (is string= "<div id='container1' class='container'><a href='/test' target='_blank'><span class='square'>caption</span><img src='/my-img-server' />&gt;&lt;</a></div>" (serialize parsed-markup))
+    (is string=  
+	"<div id='container1' class='container'>
    <a href='/test' target='_blank'>
       <span class='square'>caption
       </span>
       <img src='/my-img-server' />&gt;&lt;
    </a>
 </div>"
-      (serialize *parsed-markup* t))
-  (is string= "<account id='1'><name><first-name>Liam</first-name><second-name>Howley</second-name></name><dob><day>1</day><month>Jan</month><year>2000</year></dob></account>"
-      (serialize *parsed-account*))
-  (is string= 
-      "<account id='1'>
+	(serialize parsed-markup t))
+    (is string= "<account id='1'><name><first-name>Liam</first-name><second-name>Howley</second-name></name><dob><day>1</day><month>Jan</month><year>2000</year></dob></account>"
+	(serialize *parsed-account*))
+    (is string= 
+	"<account id='1'>
    <name>
       <first-name>Liam
       </first-name>
@@ -134,7 +133,7 @@
       </year>
    </dob>
 </account>"
-      (serialize *parsed-account* t)))
+	(serialize *parsed-account* t))))
 
 
 (define-test reader...
